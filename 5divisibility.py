58a6dc0575eb9ae58c05bb2322bb7433b391f14d
siroi97 <101795888+siroi97@users.noreply.github.com>
2022-08-16 18:13:04 +0530

    5 divisibility if _else

---
+++ 5divisibility.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Admin
+#
+# Created:     16/08/2022
+# Copyright:   (c) Admin 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+x=int(input('please enter a number number'))
+if x%5==0:
+    print('HELLO')
+else:
+    print('Bye')
+
